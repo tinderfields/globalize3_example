e17ff6bef6cfbd7ce6e60e0aa3c3256694c4181e
Paul Odeon <mail@paulodeon.com>
2011-03-25 22:32:23 +0000

    Added locale switching

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def localize_current_path(locale)
+      current_uri = request.env['PATH_INFO']
+      my_path = ActionController::Routing::Routes.recognize_path current_uri, :method => :get
+      #above works only with :get, you could pass in the current method with :method => request.method.to_sym
+      url_for(my_path.merge!(:locale => locale))
+    end
 end
