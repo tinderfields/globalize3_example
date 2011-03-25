fbee4a9121e3ea78f4c8152f1ccf17df361c80e3
Paul Odeon <mail@paulodeon.com>
2011-03-25 19:50:17 +0000

    Added Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Globalize3Example::Application.config.secret_token = '19f31541ae4f5e84c217478454de6f215e369c8265dc108ddd4eac6935475f43fb47818976c4dce41c9afb52ab1d3e17a9c8b824b2959e36861f8b54351dedbd'
